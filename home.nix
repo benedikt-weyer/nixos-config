264302dc3c3d7e7bfce9c337c5df852a4c22404b
benedikt-weyer <bw.development@pm.me>
2025-02-19 19:52:52 +0100

    feat: add Git configuration settings in home.nix

--- home.nix
+++ home.nix
@@ -386,6 +386,18 @@
         commit.gpgsign = true;
         gpg.format = "openpgp";
         user.signingkey = "09E2C230A7CCCBF5";
+        user.name = "Benedikt Weyer";
+        user.email = "bw.development@pm.me";
+        pull.rebase = true;
+        core.editor = "code --wait";
+        core.autocrlf = "input";
+        init.defaultBranch = "main";
+        diff.tool = "vscode";
+        difftool.vscode.cmd = "code --wait --diff $LOCAL $REMOTE";
+        alias.co = "checkout";
+        alias.br = "branch";
+        alias.ci = "commit";
+        alias.st = "status";
       };
     };
   };
