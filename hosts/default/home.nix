0e3145619cd206f25a16cea0d4ba48fdfea09a11
benedikt-weyer <bw.development@pm.me>
2025-03-02 22:59:28 +0100

    feat: add wl-clipboard to home configuration

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -86,6 +86,7 @@ postgresql_17
     obsidian
 
     alacritty-theme
+    wl-clipboard
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
