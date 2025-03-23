d80ce2da857422001165dbffe94c1216b086d6ed
benedikt-weyer <bw.development@pm.me>
2025-03-23 20:26:03 +0100

    feat: remove Firefox profile configurations

--- hosts/default/programs/firefox.nix
+++ hosts/default/programs/firefox.nix
@@ -79,37 +79,7 @@ in
         };
       };
 
-      /* ---- PROFILES ---- */
-      # Switch profiles via about:profiles page.
-      # For options that are available in Home-Manager see
-      # https://nix-community.github.io/home-manager/options.html#opt-programs.firefox.profiles
-      profiles ={
-        profile_0 = {           # choose a profile name; directory is /home/<user>/.mozilla/firefox/profile_0
-          id = 0;               # 0 is the default profile; see also option "isDefault"
-          name = "profile_0";   # name as listed in about:profiles
-          isDefault = true;     # can be omitted; true if profile ID is 0
-          settings = {          # specify profile-specific preferences here; check about:config for options
-            "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-            "browser.startup.homepage" = "https://nixos.org";
-            "browser.newtabpage.pinned" = [{
-              title = "NixOS";
-              url = "https://nixos.org";
-            }];
-            # add preferences for profile_0 here...
-          };
-        };
-        profile_1 = {
-          id = 1;
-          name = "profile_1";
-          isDefault = false;
-          settings = {
-            "browser.newtabpage.activity-stream.feeds.section.highlights" = true;
-            "browser.startup.homepage" = "https://ecosia.org";
-            # add preferences for profile_1 here...
-          };
-        };
-      # add profiles here...
-      };
+
     };
   };
 }
