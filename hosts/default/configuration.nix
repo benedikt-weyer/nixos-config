cc0073dd29d49d229b028fe04015e503c54684ff
benedikt-weyer <bw.development@pm.me>
2025-03-23 19:59:54 +0100

    feat: add pcsclite and pcsc-tools to configuration and enable pcscd service

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -217,7 +217,12 @@
     #  wget
     # home-manager
     cifs-utils
+
+    pcsclite
+    pcsc-tools
   ];
+
+  services.pcscd.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
