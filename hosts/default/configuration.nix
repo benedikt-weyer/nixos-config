3b79c82189b16c579fce636692cebece18a4b461
benedikt-weyer <bw.development@pm.me>
2025-04-09 10:33:10 +0200

    feat: enable Steam and Gamescope session in configuration.nix

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -207,6 +207,15 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  programs.steam.gamescopeSession.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
