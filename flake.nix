ab4ffd924d1b593dd860c3f50f1e0772c50c7700
Benedikt Weyer <bw.development@pm.me>
2025-05-21 18:29:41 +0200

    feat: comment out libfprint and fprintd overrides in overlays

--- flake.nix
+++ flake.nix
@@ -73,22 +73,22 @@
               overlays = [
                 (final: prev:
                 {
-                  libfprint = prev.libfprint.overrideAttrs (old: {
-                    src = builtins.fetchGit {
-                      url = "https://gitlab.freedesktop.org/depau/libfprint.git";
-                      ref = "elanmoc2";
-                      rev = "f4439ce96b2938fea8d4f42223d7faea05bd4048";
-                    };
-                  });
+                  # libfprint = prev.libfprint.overrideAttrs (old: {
+                  #   src = builtins.fetchGit {
+                  #     url = "https://gitlab.freedesktop.org/depau/libfprint.git";
+                  #     ref = "elanmoc2";
+                  #     rev = "f4439ce96b2938fea8d4f42223d7faea05bd4048";
+                  #   };
+                  # });
 
-                  fprintd = prev.fprintd.overrideAttrs (old: {
-                    mesonCheckFlags = [
-                      # PAM related checks are timing out
-                      "--no-suite" "fprintd:TestPamFprintd" 
-                      # Tests FPrintdManagerPreStartTests.test_manager_get_no_default_device & FPrintdManagerPreStartTests.test_manager_get_no_devices are failing
-                      "--no-suite" "fprintd:FPrintdManagerPreStartTests"
-                    ];
-                  });
+                  # fprintd = prev.fprintd.overrideAttrs (old: {
+                  #   mesonCheckFlags = [
+                  #     # PAM related checks are timing out
+                  #     "--no-suite" "fprintd:TestPamFprintd" 
+                  #     # Tests FPrintdManagerPreStartTests.test_manager_get_no_default_device & FPrintdManagerPreStartTests.test_manager_get_no_devices are failing
+                  #     "--no-suite" "fprintd:FPrintdManagerPreStartTests"
+                  #   ];
+                  # });
                 })
               ];
             };
