721cb72d1abaf03019afc06cfd4cd3fe0f1166c2
Benedikt Weyer <bw.development@pm.me>
2025-05-26 19:51:16 +0200

    fix: correct pinentryPackage to pinentry.package in gpg-agent configuration

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -60,7 +60,7 @@
 
   services.gpg-agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-gnome3;
   };
 
   fonts.fontconfig.enable = true;
